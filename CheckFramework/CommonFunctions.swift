69e4d9b1cdc06ebc411b89020bacea2b0657c9db
Satyendra singh <satyendra.bhati@neosoftmail.com>
2021-09-28 12:19:55 +0530

    Initial commit

---
+++ CheckFramework/CommonFunctions.swift
@@ -0,0 +1,14 @@
+//
+//  CommonFunctions.swift
+//  TestALMFramework
+//
+//  Created by Neosoft on 27/09/21.
+//
+
+import Foundation
+
+class Methods{
+    static func printHello() -> String{
+        return "Hello World!!!"
+    }
+}
