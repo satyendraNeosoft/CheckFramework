503a9fe1a3fda9fcba9609c6e52b34fcbc4a3abf
aplyamukesh <mukesh.neosoft@apalya.com>
2021-09-28 12:07:23 +0530

    Initial Commit

---
+++ CheckFramework/CheckFramework.h
@@ -0,0 +1,18 @@
+//
+//  CheckFramework.h
+//  CheckFramework
+//
+//  Created by Neosoft on 28/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckFramework.
+FOUNDATION_EXPORT double CheckFrameworkVersionNumber;
+
+//! Project version string for CheckFramework.
+FOUNDATION_EXPORT const unsigned char CheckFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckFramework/PublicHeader.h>
+
+
